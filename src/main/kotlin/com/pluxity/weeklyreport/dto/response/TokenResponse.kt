d1d7709d4e25f89307ca1b1eb4dbb884551b472b
inyeop <kim41516@pluxity.com>
2026-02-12 11:08:58 +0900

    feat: 토큰 응답에 userId 필드 추가
    - TokenResponse DTO에 userId 필드 추가로 사용자 정보 포함
    - AuthService에서 토큰 생성 시 userId를 응답에 포함하여 클라이언트에서 사용자 구분 가능

--- src/main/kotlin/com/pluxity/weeklyreport/dto/response/TokenResponse.kt
+++ src/main/kotlin/com/pluxity/weeklyreport/dto/response/TokenResponse.kt
@@ -3,5 +3,6 @@ package com.pluxity.weeklyreport.dto.response
 data class TokenResponse(
     val accessToken: String,
     val tokenType: String = "Bearer",
-    val expiresIn: Long
+    val expiresIn: Long,
+    val userId: Long
 )
